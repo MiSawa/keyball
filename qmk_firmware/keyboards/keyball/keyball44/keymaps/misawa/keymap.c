de0e2ea33fc3534213ab8da86a2bc95d05fd924f
mi_sawa <mi.sawa.1216+git@gmail.com>
2023-08-11 22:25:54 +0900

    Fix compile errors

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/misawa/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/misawa/keymap.c
@@ -29,8 +29,12 @@ enum layers {
   LAYER_ADJUST,
 };
 
-#define _______ KC_TRNS
-#define XXXXXXX KC_NO
+#ifndef _______
+#  define _______ KC_TRNS
+#endif
+#ifndef XXXXXXX
+#  define XXXXXXX KC_NO
+#endif
 
 #define MO_LOWER MO(LAYER_LOWER)
 #define MO_RAISE MO(LAYER_UPPER)
@@ -55,42 +59,42 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB   , KC_Q     , KC_D     , KC_R     , KC_W     , KC_B     ,                                        KC_J     , KC_F     , KC_U     , KC_P     , KC_SCLN  , KC_BSLS  ,
     KC_LCTL  , KC_A     , KC_S     , KC_H     , KC_T     , KC_G     ,                                        KC_Y     , KC_N     , KC_E     , KC_O     , KC_I     , KC_QUOT  ,
     KC_LSFT  , KC_Z     , KC_X     , KC_M     , KC_C     , KC_V     ,                                        KC_K     , KC_L     , KC_COMM  , KC_DOT   , KC_SLSH  , KC_RSFT  ,
-                          LALTMHEN , LCTLHENK      , KC_BSPC  , MO_LOWER , KC_SPC   ,                   KC_ENT   , MO_RAISE ,                          , MO_MOUSE
+                          LALTMHEN , LCTLHENK      , KC_BSPC  , MO_LOWER , KC_SPC   ,                   KC_ENT   , MO_RAISE ,                            MO_MOUSE
   ),
 
   [LAYER_QWERTY] = LAYOUT_right_ball(
     KC_TAB   , KC_Q     , KC_W     , KC_E     , KC_R     , KC_T     ,                                        KC_Y     , KC_U     , KC_I     , KC_O     , KC_P     , KC_BSLS  ,
     KC_LCTL  , KC_A     , KC_S     , KC_D     , KC_F     , KC_G     ,                                        KC_H     , KC_J     , KC_K     , KC_L     , KC_SCLN  , KC_QUOT  ,
     KC_LSFT  , KC_Z     , KC_X     , KC_C     , KC_V     , KC_B     ,                                        KC_N     , KC_M     , KC_COMM  , KC_DOT   , KC_SLSH  , KC_RSFT  ,
-                          LALTMHEN , LCTLHENK      , KC_BSPC  , MO_LOWER , KC_SPC   ,                   KC_ENT   , MO_RAISE ,                          , MO_MOUSE
+                          LALTMHEN , LCTLHENK      , KC_BSPC  , MO_LOWER , KC_SPC   ,                   KC_ENT   , MO_RAISE ,                            MO_MOUSE
   ),
 
   [LAYER_LOWER] = LAYOUT_right_ball(
     KC_ESC   , KC_EXLM  , KC_AT    , KC_HASH  , KC_DLR   , KC_PERC  ,                                        KC_CIRC  , KC_AMPR  , KC_ASTR  , KC_LPRN  , KC_RPRN  , KC_DEL   ,
     _______  , XXXXXXX  , XXXXXXX  , XXXXXXX  , XXXXXXX  , XXXXXXX  ,                                        KC_GRV   , KC_MINUS , KC_EQL   , KC_LCBR  , KC_RCBR  , KC_GRV   ,
     _______  , XXXXXXX  , XXXXXXX  , XXXXXXX  , XXXXXXX  , XXXXXXX  ,                                        KC_TILD  , KC_UNDS  , KC_PLUS  , KC_LBRC  , KC_RBRC  , KC_TILD  ,
-                          _______  , _______       , _______  , _______  , _______  ,                   _______  , _______  ,                          , _______
+                          _______  , _______       , _______  , _______  , _______  ,                   _______  , _______  ,                            _______
   ),
 
   [LAYER_UPPER] = LAYOUT_right_ball(
     _______  , KC_F1    , KC_F2    , KC_F3    , KC_F4    , KC_F5    ,                                        KC_F6    , KC_F7    , KC_F8    , KC_F9    , KC_F10   , KC_F11   ,
     _______  , KC_1     , KC_2     , KC_3     , KC_4     , KC_5     ,                                        KC_6     , KC_7     , KC_8     , KC_9     , KC_0     , KC_F12   ,
     _______  , XXXXXXX  , XXXXXXX  , XXXXXXX  , XXXXXXX  , XXXXXXX  ,                                        KC_LEFT  , KC_DOWN  , KC_UP    , KC_RIGHT , XXXXXXX  , _______  ,
-                          _______  , _______       , _______  , _______  , _______  ,                   _______  , _______  ,                          , _______
+                          _______  , _______       , _______  , _______  , _______  ,                   _______  , _______  ,                            _______
   ),
 
   [LAYER_MOUSE] = LAYOUT_right_ball(
     _______  , XXXXXXX  , XXXXXXX  , XXXXXXX  , XXXXXXX  , XXXXXXX  ,                                        XXXXXXX  , XXXXXXX  , SCRL_MO  , XXXXXXX  , XXXXXXX  , XXXXXXX  ,
     _______  , XXXXXXX  , XXXXXXX  , XXXXXXX  , XXXXXXX  , XXXXXXX  ,                                        XXXXXXX  , KC_BTN1  , KC_BTN2  , KC_BTN3  , XXXXXXX  , XXXXXXX  ,
     _______  , XXXXXXX  , XXXXXXX  , XXXXXXX  , XXXXXXX  , XXXXXXX  ,                                        XXXXXXX  , XXXXXXX  , XXXXXXX  , XXXXXXX  , XXXXXXX  , _______  ,
-                          _______  , _______       , _______  , _______  , _______  ,                   _______  , _______  ,                          , _______
+                          _______  , _______       , _______  , _______  , _______  ,                   _______  , _______  ,                            _______
   ),
 
   [LAYER_ADJUST] = LAYOUT_right_ball(
     XXXXXXX  , XXXXXXX  , XXXXXXX  , XXXXXXX  , XXXXXXX  , XXXXXXX  ,                                        DF_WORK  , DF_QWERT , XXXXXXX  , XXXXXXX  , XXXXXXX  , XXXXXXX  ,
     XXXXXXX  , XXXXXXX  , XXXXXXX  , XXXXXXX  , XXXXXXX  , XXXXXXX  ,                                        XXXXXXX  , SCRL_DVD , SCRL_DVI , XXXXXXX  , XXXXXXX  , XXXXXXX  ,
     XXXXXXX  , XXXXXXX  , XXXXXXX  , XXXXXXX  , XXXXXXX  , XXXXXXX  ,                                        CPI_D1K  , CPI_D100 , CPI_I100 , CPI_I1K  , XXXXXXX  , KBC_SAVE ,
-                          RESET    , KBC_RST       , XXXXXXX  , _______  , XXXXXXX  ,                   XXXXXXX  , _______  ,                          , XXXXXXX
+                          RESET    , KBC_RST       , XXXXXXX  , _______  , XXXXXXX  ,                   XXXXXXX  , _______  ,                            XXXXXXX
   ),
 };
 // clang-format on
